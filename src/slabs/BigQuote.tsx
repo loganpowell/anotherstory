470573ec7ebdc5211ec8a3ef41fb2067803f1c86
Logan Powell <loganp@tepper.cmu.edu>
2021-10-27 22:43:00 -0400

    folding up

--- src/slabs/BigQuote.tsx
+++ src/slabs/BigQuote.tsx
@@ -8,7 +8,7 @@ export const BigQuote = () => {
     return (
         <Slab bg="light_5" padding={xtall_slab_padding}>
             <AvatarQuote
-                src="https://media-exp1.licdn.com/dms/image/C4E03AQGkR9oycFOYYA/profile-displayphoto-shrink_200_200/0/1517723992109?e=1640822400&v=beta&t=TF-TQv6g7R_SW9eEDcL45xiFphzrFadaus3Qu9jScXc"
+                src="https://pbs.twimg.com/profile_images/1391714126/Me_400x400.png"
                 color="dark_5"
                 text="[This] team has more vision, imagination and problem solving ability than all five previous architects (combined) with whom we have done projects. their design-build approach simplifies multiple meetings and miscommunications between a separate contractor and architect. The end result of our renovation is stunning."
                 name="Sheri Spain Long"
