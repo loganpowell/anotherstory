c6b6b7022671898a481aabcf64c84c4b91225948
Logan Powell <loganp@tepper.cmu.edu>
2021-10-27 23:17:01 -0400

    buies

--- src/slabs/BannerQuote.tsx
+++ src/slabs/BannerQuote.tsx
@@ -20,7 +20,7 @@ export const BannerQuote = ({
     shape = "circle",
     align = "left",
     topline = "The Buies",
-    banner_img = "https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+    banner_img = "https://anotherstorypub.s3.amazonaws.com/PA210211.JPG",
 }) => {
     const { fontWeights, fontSizes, letterSpacings, colors } = useMyTheme()
     const position = {
