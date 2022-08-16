0bc7aed1b87e55e839891773aec51f253c0e8553
Logan Powell <loganp@tepper.cmu.edu>
2022-08-16 08:09:04 -0400

    adds clarity snippet

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-export default ({ mode }) => {
+const config = ({ mode }) => {
     return defineConfig({
         plugins: [
             react({
@@ -22,3 +22,5 @@ export default ({ mode }) => {
         },
     })
 }
+
+export default config
